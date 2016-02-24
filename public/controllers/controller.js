29381381bc645699a34f0651f8ec4975080bc35a
vanbumi <jsp.dio@gmail.com>
2016-02-24 21:27:42 +0700

    update commit

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,16 +4,22 @@ var myApp = angular.module('myApp',[]);
 myApp.controller('AppCtrl', function ($scope, $http) {
   console.log("Hello from controller");
 
+var refresh = function() {
   $http.get('/contactlist').success(function(response) {
       console.log("I got the data I requested");
       $scope.contactlist = response;
+      $scope.contact = "";
   });
+};
+
+refresh();
 
   $scope.addContact = function() {
       console.log($scope.contact);
       //send input data to server
       $http.post('/contactlist', $scope.contact).success(function(response) {
           console.log(response);
+          refresh();
       });
 
   };
